834bb221e5b449202e07e0e59a55a52eed1b937e
Jason <j.vanschaik@tcrmbo.nl>
2025-03-05 10:24:05 +0100

    start lesson 1

--- lesson01/app/index.jsx
+++ lesson01/app/index.jsx
@@ -1,16 +1,24 @@
-import { View, Text, Button } from "react-native";
-import { useRouter } from "expo-router";
+import { View, Text, StyleSheet} from "react-native";
 
 
 export default function App() {
-  const router = useRouter();
+ 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24 }}>👋 Welkom!</Text>
-      <Button title="Inloggen" onPress={() => router.push("/screens/auth/login")} />
-      <Button title="Registreren" onPress={() => router.push("/screens/auth/register")} />
-      <Button title="Verder naar de app" onPress={() => router.replace("/screens/tabs/home")} />
+    <View style={styles.container}>
+      <Text style={styles.text}>👋 Welkom!</Text>
+    
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 24
+  }
+
+});
